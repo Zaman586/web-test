f888f10a4e1816ea19534a34904757eb40fdf6b9
Zamananwar <zamananwar856@gmail.com>
2025-04-25 19:50:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static(__dirname)); // Serve static files
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '/')));
+
+// Fallback to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
