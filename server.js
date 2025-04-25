ce603879c0ddbb3cdb318a7edd1f321b9ad70460
Zaman Anwar <zamananwar856@gmail.com>
2025-04-25 19:45:52 +0500

    Added working server.js and completed app structure

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname)); // Serve static files
+app.listen(3000, () => console.log('Server running on port 3000'));
